c0f3effd2d0f1a03255fa4fd04b2074599fc1b63
YuraStruminskiy <ystruminskiy@gmail.com>
2023-11-30 15:30:32 +0200

    fixed Nav component

--- react-app/src/Components/Nav/Nav.jsx
+++ react-app/src/Components/Nav/Nav.jsx
@@ -6,11 +6,11 @@ const Nav = () => {
     <nav className='nav'>
         <h2 className="nav__title">React.</h2>
         <div className="nav__wrapper">
-            <a href="#" className="nav__link">Home</a>
-            <a href="#" className="nav__link">Company</a>
-            <a href="#" className="nav__link">Resources</a>
-            <a href="#" className="nav__link">About</a>
-            <a href="#" className="nav__link">Contact</a>
+            <a href="/#" className="nav__link">Home</a>
+            <a href="/#" className="nav__link">Company</a>
+            <a href="/#" className="nav__link">Resources</a>
+            <a href="/#" className="nav__link">About</a>
+            <a href="/#" className="nav__link">Contact</a>
         </div>
     </nav>
   )
