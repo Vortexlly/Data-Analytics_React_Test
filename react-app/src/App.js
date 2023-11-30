e887f056b0b210234f1159da36222e527d0a53f4
YuraStruminskiy <ystruminskiy@gmail.com>
2023-11-30 15:08:28 +0200

    Creating Trial component #5

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,18 @@
+import './App.css';
+import TrialItem from "./Components/Trial/TrialItem";
+import { trialData1, trialData2, trialData3 } from './Components/Trial/trialData';
 
 function App() {
+
+
   return (
-    <div>
-      
-    </div>
+    <main>
+      <section className="trial">
+        <TrialItem data={trialData1}/>
+        <TrialItem data={trialData2}/>
+        <TrialItem data={trialData3}/>
+      </section>
+    </main>
   );
 }
 
---
+++ react-app/src/Components/Trial/trialData.js
@@ -0,0 +1,29 @@
+import img from './trial-icon.png';
+
+export const trialData1 = {
+    name: 'Single user',
+    price: 140,
+    storage: 500,
+    users: 1,
+    sending: 2,
+    url: img,
+    alt: 'person icon'
+}
+export const trialData2 = {
+    name: 'Two users',
+    price: 240,
+    storage: 500,
+    users: 2,
+    sending: 4,
+    url: img,
+    alt: 'person icon'
+}
+export const trialData3 = {
+    name: 'Three user',
+    price: 440,
+    storage: 1000,
+    users: 3,
+    sending: 10,
+    url: img,
+    alt: 'person icon'
+}
